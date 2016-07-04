bdd3c2c257b491dea39d8db934b30265533070ae
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2016-07-04 18:11:15 +0100

    first commit

---
+++ lib/sidekiq/logstash.rb
@@ -0,0 +1,7 @@
+require "sidekiq/logstash/version"
+
+module Sidekiq
+  module Logstash
+    # Your code goes here...
+  end
+end
---
+++ lib/sidekiq/logstash/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module Logstash
+    VERSION = "0.1.0"
+  end
+end
